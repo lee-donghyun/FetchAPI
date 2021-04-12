487638d768842ec1063aeb4f6c6c3617075f660c
lee dong hyun <19011673@sju.ac.kr>
2021-04-12 23:51:45 +0900

    log in in https

--- kakao.js
+++ kakao.js
@@ -1,15 +1,23 @@
 let root = document.getElementById('root');
-import 'https://developers.kakao.com/sdk/js/kakao.js';
 
 Kakao.init('df03ab79eefead95a87e571c7eb171d8');
 console.log(Kakao.isInitialized());
 
 
 
-
+const loginWithKakao = () => {
+    Kakao.Auth.login({
+        success: function (authObj) {
+            alert(JSON.stringify(authObj))
+        },
+        fail: function (err) {
+            alert(JSON.stringify(err))
+        },
+    })
+}
 
 const hello = () => {
-    alert('hello')
+    loginWithKakao();
 }
 
 
