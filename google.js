426910997f534bd7db94561d403d5211697a0c1d
lee dong hyun <19011673@sju.ac.kr>
2021-05-05 19:09:08 +0900

    google log in page show

---
+++ google.js
@@ -0,0 +1,21 @@
+let GoogleAuth; // Google Auth object.
+const initClient = async () => {
+    await gapi.client.init({
+        'apiKey': '258563955545-o3qj8nebt6mar2qnbrkortvb2beocgim.apps.googleusercontent.com',
+        'clientId': 't9uE4Txc1PfKnxR0LNLtVzYQ',
+        'scope': 'https://mail.google.com/',
+        'discoveryDocs': ['https://gmail.googleapis.com/$discovery/rest?version=v1']
+    });
+
+    GoogleAuth = gapi.auth2.getAuthInstance();
+    GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+}
+const onclickButton = async () => {
+    console.log('init google gapi');
+    gapi.load('client:auth2', initClient);
+}
+const onclicktest = () => {
+    console.log('log in');
+    GoogleAuth.signIn();
+}
